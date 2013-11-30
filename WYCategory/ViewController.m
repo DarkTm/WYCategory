d847691fffb393af15b0fbcae2710bbdddbac65a
qiaohe <tusc_heqiao@163.com>
2013-11-30 16:38:15 +0800

    new

---
+++ WYCategory/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  WYCategory
+//
+//  Created by 3TI on 13-11-30.
+//  Copyright (c) 2013年 qiaquan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "WYCategory/WYCategory.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
